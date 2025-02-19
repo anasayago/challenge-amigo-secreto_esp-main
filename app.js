8a4df0a6fc373727dc835f8740258682d072bad3
anasayago <anasayago@gmail.com>
2025-02-19 12:26:43 +0100

    Mostrar lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let listaAmigos = document.getElementById("listaAmigos");
+
+function agregarAmigo(){
+    var texto = document.getElementById("amigo").value;
+    if (texto.trim() === "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(texto);
+        mostrarAmigos()
+        document.getElementById("amigo").value = "";
+    }
+ }
+
+ function mostrarAmigos(){
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += "<li>" + amigos[i] + "</li>";
+    }
+ }
